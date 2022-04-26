4607293cebdc58deb098271a9e282b0485460346
double_s <sz.suchodolski@gmail.com>
2022-04-26 21:08:20 +0200

    testing screen height in case of strange resizing effect

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const chordsModal = document.querySelector('.chords');
 const closeChords = document.querySelector('.chords .close');
 
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh' `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh' `${vh}px`);
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    console.log(vh);
 });
 
 
