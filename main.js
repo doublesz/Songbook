4c6c3b28d33af323ab319a7ededdac6e7a9b78f7
double_s <sz.suchodolski@gmail.com>
2022-04-29 19:02:44 +0200

    delete unnecesary comments

--- main.js
+++ main.js
@@ -2,15 +2,6 @@ const chordsBtn = document.querySelector('.chords-btn');
 const chordsModal = document.querySelector('.chords');
 const closeChords = document.querySelector('.chords .close');
 
-// let vh = window.innerHeight * 0.01;
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// window.addEventListener('resize', () => {
-//     let vh = window.innerHeight * 0.01;
-//     document.documentElement.style.setProperty('--vh', `${vh}px`);
-// });
-
-
 const showChords = () => {
     chordsModal.style.display = 'initial';
 }
