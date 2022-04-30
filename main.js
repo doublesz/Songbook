110c728b6b0fc3d8f7aa351826d6cfbe74d6ace5
double_s <sz.suchodolski@gmail.com>
2022-04-30 11:34:30 +0200

    working on toggling search section on desktop

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ const chordsBtn = document.querySelector('.chords-btn');
 const chordsModal = document.querySelector('.chords');
 const closeChords = document.querySelector('.chords .close');
 
+//to do
+// const tabsBtn = document.querySelector('.tabs-btn');
+// const tabsModal = document.querySelector('.tabs');
+// const tabsChords = document.querySelector('.tabs .close');
+
+const toggleSearch = document.querySelector('.toggle-search');
+const searchSection = document.querySelector('.search-section');
+
+
+const toggleSearchSection = () => {
+    isSearchSectionDisplayNone() ? searchSection.style.display = 'inherit' : searchSection.style.display = 'none';
+}
+
+const isSearchSectionDisplayNone = () => {
+    const displayProp = window.getComputedStyle(searchSection).getPropertyValue('display');
+    return displayProp === 'none';
+}
+
+toggleSearch.addEventListener('click', toggleSearchSection);
+
+
 const showChords = () => {
     chordsModal.style.display = 'initial';
 }
